73ae8de1c4901a3296471266660336e79f9cfd9e
ihassaanadeem <mnadeem.bscs24seecs@seecs.edu.pk>
2026-05-26 20:28:01 +0500

    feat: implement forward propagation

--- network.py
+++ network.py
@@ -8,20 +8,35 @@ class NeuralNetwork():
         
         # kaiming initialization compensates for 50% variance drop due to ReLU activation
         kaiming_w1 = np.sqrt(2 / self.input_size)
-        self.w1 = np.random.randn(self.input_size , self.hidden_size) * kaiming_w1
-        self.b1 = np.zeros((1,self.hidden_size))
+        self.w1 = np.random.randn(self.hidden_size, self.input_size) * kaiming_w1
+        self.b1 = np.zeros((self.hidden_size, 1))
         
         kaiming_w2 = np.sqrt(2 / self.hidden_size)
-        self.w2 = np.random.randn(self.hidden_size, self.output_size) * kaiming_w2
-        self.b2 = np.zeros((1, self.output_size))
+        self.w2 = np.random.randn(self.output_size, self.hidden_size) * kaiming_w2
+        self.b2 = np.zeros((self.output_size, 1))
         
+        self.forward_prop(np.random.randn(self.input_size, 1))
+    
     def forward_prop(self, data):
+        d1 = np.dot(self.w1, data) + self.b1
+        a1 = np.maximum(0, d1) #ReLU activation
         
+        # logits
+        d2 = np.dot(self.w2, a1) + self.b2
+        
+        # softmax activation function
+        a2 = np.exp(d2)
+        a2 = a2 / np.sum(a2)
+        
+        # print(a2.shape)
+        
+        return a1, a2
     
     def loss_func():
-        pass
+        return
     
     def back_prop():
-        pass
+        return
     
     
+nn = NeuralNetwork(784, 128, 10)
