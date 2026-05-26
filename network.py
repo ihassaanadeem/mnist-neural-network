c8bb8999711226abbd5054ef741b82d3aef3b2e2
ihassaanadeem <mnadeem.bscs24seecs@seecs.edu.pk>
2026-05-26 21:09:18 +0500

    feat: add batch processing

--- network.py
+++ network.py
@@ -1,34 +1,33 @@
 import numpy as np
 
 class NeuralNetwork():
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, batch_size):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
+        self.batch_size = batch_size
         
         # kaiming initialization compensates for 50% variance drop due to ReLU activation
         kaiming_w1 = np.sqrt(2 / self.input_size)
-        self.w1 = np.random.randn(self.hidden_size, self.input_size) * kaiming_w1
-        self.b1 = np.zeros((self.hidden_size, 1))
+        self.w1 = np.random.randn(self.input_size, self.hidden_size) * kaiming_w1
+        self.b1 = np.zeros((1, self.hidden_size))
         
         kaiming_w2 = np.sqrt(2 / self.hidden_size)
-        self.w2 = np.random.randn(self.output_size, self.hidden_size) * kaiming_w2
-        self.b2 = np.zeros((self.output_size, 1))
+        self.w2 = np.random.randn(self.hidden_size, self.output_size) * kaiming_w2
+        self.b2 = np.zeros((1, self.output_size))
         
-        self.forward_prop(np.random.randn(self.input_size, 1))
+        self.forward_prop(np.random.randn(self.batch_size, self.input_size))
     
     def forward_prop(self, data):
-        d1 = np.dot(self.w1, data) + self.b1
+        d1 = np.dot(data, self.w1) + self.b1
         a1 = np.maximum(0, d1) #ReLU activation
         
         # logits
-        d2 = np.dot(self.w2, a1) + self.b2
+        d2 = np.dot(a1, self.w2) + self.b2
         
         # softmax activation function
         a2 = np.exp(d2)
-        a2 = a2 / np.sum(a2)
-        
-        # print(a2.shape)
+        a2 = a2 / np.sum(a2, axis=1, keepdims=True)
         
         return a1, a2
     
@@ -39,4 +38,4 @@ class NeuralNetwork():
         return
     
     
-nn = NeuralNetwork(784, 128, 10)
+nn = NeuralNetwork(784, 128, 10, 50)
