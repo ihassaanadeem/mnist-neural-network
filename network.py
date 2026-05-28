7accb546253b2f919ab87e45bbd258c80601ada6
ihassaanadeem <mnadeem.bscs24seecs@seecs.edu.pk>
2026-05-28 19:01:08 +0500

    fix: skeleton update

--- network.py
+++ network.py
@@ -40,7 +40,7 @@ class NeuralNetwork():
     
     def loss_func(self, labels):
         # cross-entropy loss calc
-        probs = self.a2[np.arange(self.batch_size), labels]
+        probs = self.a2[np.arange(len(labels)), labels]
         loss = -np.log(probs + 1e-15)
         
         # mean loss over batch
@@ -52,20 +52,17 @@ class NeuralNetwork():
         # chain rule to calculate derivatives starting from predicted vals
         self.grad_d2 = self.a2.copy()
         self.grad_d2[np.arange(len(labels)), labels] -= 1
-        self.grad_d2 /= self.batch_size
         
-        self.grad_w2 = np.dot(self.a1.T, self.grad_d2)
-        self.grad_b2 = np.sum(self.grad_d2, axis=0, keepdims=True)
+        self.grad_w2 = np.dot(self.a1.T, self.grad_d2) / len(labels)
+        self.grad_b2 = np.sum(self.grad_d2, axis=0, keepdims=True) / len(labels)
         
         self.grad_a1 = np.dot(self.grad_d2, self.w2.T)
         
         self.grad_d1 = self.grad_a1.copy()
-        self.grad_d1[self.a1<0] = 0
+        self.grad_d1[self.a1 <= 0] = 0
         
-        self.grad_w1 = np.dot(data.T, self.grad_d1)
-        self.grad_b1 = np.sum(self.grad_d1, axis=0, keepdims=True)
-        
-        return
+        self.grad_w1 = np.dot(data.T, self.grad_d1) / len(labels)
+        self.grad_b1 = np.sum(self.grad_d1, axis=0, keepdims=True) / len(labels)
         
     def update_params(self, learning_rate):
         # update rule: 
@@ -74,3 +71,9 @@ class NeuralNetwork():
         self.b1 -= learning_rate * self.grad_b1
         self.w2 -= learning_rate * self.grad_w2
         self.b2 -= learning_rate * self.grad_b2
+
+    def calc_accuracy(self, labels):
+        predictions = np.argmax(self.a2, axis=1)
+        accuracy = np.mean(predictions == labels)
+        
+        return accuracy
